c681d5c2d8167c5836b0b904eb59d5def5813099
Ponmile Adebisi <ponmileadebisi@gmail.com>
2023-03-15 20:30:50 +0100

    Nofound and Home component added

---
+++ src/components/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Page Not Found</h1>
+  </div>
+</template> 
+
+<script>
+export default {
+  name: 'NotFound',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
